bcac63b0ae43231653ce2b9567bff1bf51efa5ca
equusligneus <reher.frederik@gmail.com>
2025-01-24 22:50:08 +0100

    Recreated what was lost in the fire

--- Script/Ammo/ammo_pickup.gd
+++ Script/Ammo/ammo_pickup.gd
@@ -4,10 +4,14 @@ extends Node
 @export var _type : AmmoType
 @export var _count := 5 
 
+@onready var _sprite : Sprite3D = $CollisionShape3D/Sprite3D
+
+func _ready():
+	_sprite.texture = _type.get_icon()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	_sprite.rotate(Vector3.UP, 15.0 * delta)
 
 func _on_body_entered(body: Node3D) -> void:
 	# check is player
