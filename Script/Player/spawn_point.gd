f93fe8702dc2c090b51e0402a5db98dc4ebd75db
equusligneus <reher.frederik@gmail.com>
2025-01-25 16:58:44 +0100

    fixed spawn overriding scale

--- Script/Player/spawn_point.gd
+++ Script/Player/spawn_point.gd
@@ -3,10 +3,14 @@ extends Node3D
 @export var scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:	
+func _ready() -> void:
 	await get_tree().process_frame
 	
 	var node : Node3D = scene.instantiate()
 	node.position = global_position
-	node.basis = global_basis
+	var new_basis := global_basis
+	new_basis.x *= node.scale.x
+	new_basis.y *= node.scale.y
+	new_basis.z *= node.scale.z
+	node.basis = new_basis
 	get_parent_node_3d().add_child(node)
