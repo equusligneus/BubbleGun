adeeecbfd2f5bd8878e06b9b6c03c2721e0b261d
equusligneus <reher.frederik@gmail.com>
2025-01-25 16:29:06 +0100

    fixed player spawn

--- Script/Player/spawn_point.gd
+++ Script/Player/spawn_point.gd
@@ -3,8 +3,10 @@ extends Node3D
 @export var scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready() -> void:	
+	await get_tree().process_frame
+	
 	var node : Node3D = scene.instantiate()
 	node.position = global_position
 	node.basis = global_basis
-	get_tree().current_scene.add_child(node)
+	get_parent_node_3d().add_child(node)
