309140ddedb724b19ca0368b1a4b6bcc24c4c16f
equusligneus <reher.frederik@gmail.com>
2025-01-25 08:41:05 +0100

    Added ground

---
+++ Script/Ground/ground.gd
@@ -0,0 +1,32 @@
+class_name Ground
+extends RefCounted
+
+var basis: Basis
+var friction: float = 0.0
+var bounciness: float = 0.0
+var is_grounded: bool = false
+const MIN_GROUND_NORMAL_Y := 0.7
+
+func _init(character: CharacterBody3D):
+	basis = character.transform.basis
+	if !character.is_on_floor():
+		return
+		
+	for i in character.get_slide_collision_count():
+		var collision := character.get_slide_collision(i)
+		var collider := collision.get_collider() as StaticBody3D
+		if collider == null:
+			continue
+		 
+		var normal := collision.get_normal()
+		if normal.y < MIN_GROUND_NORMAL_Y:
+			continue
+		
+		is_grounded = true
+		
+		var right := -(normal.cross(basis.z))
+		var forward := normal.cross(right)
+		basis = Basis(right, normal, forward)
+		var phys_mat := collider.physics_material_override
+		friction = phys_mat.friction
+		bounciness = phys_mat.bounce
