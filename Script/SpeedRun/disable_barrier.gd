d9e4425e73fc9234f4d2b4c8f12aedae25cc1adc
b34s7m0d3 <peter@petermehlmann.com>
2025-01-26 15:12:15 +0100

    Fred please do not look at the disable_barrier.gd script

--- Script/SpeedRun/disable_barrier.gd
+++ Script/SpeedRun/disable_barrier.gd
@@ -13,17 +13,26 @@ func _on_body_entered(body: Node3D):
 	if body is Gunner:
 		_shape.disabled = true
 		for barrier in _barriers:
-			barrier.disabled = true
-			barrier.get_parent_node_3d().visible = false
-		if _reenable_after > 0.0:
-			_tween = create_tween()
-			_tween.tween_interval(_reenable_after)
-			_tween.tween_callback(_reenable)
-		elif _temp_worker != null:
-			_temp_worker._tween.kill()
-
-func _reenable():
-	_shape.disabled = false
-	for barrier in _barriers:
-		barrier.disabled = false
-		barrier.get_parent_node_3d().visible = true
+			barrier.get_parent().get_parent().get_parent().queue_free()
+			
+		queue_free();
+			#barrier.disabled = true
+			#barrier.physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_OFF
+			#barrier.get_parent_node_3d().visible = false
+			#barrier.get_parent().get_parent().visible = false;
+			#barrier.get_parent().get_parent().get_parent().visible = false;
+		#if _reenable_after > 0.0:
+			#_tween = create_tween()
+			#_tween.tween_interval(_reenable_after)
+			#_tween.tween_callback(_reenable)
+		#elif _temp_worker != null:
+			#_temp_worker._tween.kill()
+#
+#func _reenable():
+	#_shape.disabled = false
+	#for barrier in _barriers:
+		#barrier.disabled = false
+		#barrier.physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_ON
+		#barrier.get_parent_node_3d().visible = true
+		#barrier.get_parent().get_parent().visible = true;
+		#barrier.get_parent().get_parent().get_parent().visible = true;
