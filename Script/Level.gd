b915a8fdb060bff03ca7c29da0e08183d3698817
equusligneus <reher.frederik@gmail.com>
2025-01-26 11:21:32 +0100

    stopped the clocks

--- Script/Level.gd
+++ Script/Level.gd
@@ -1,8 +1,7 @@
 extends Node3D
 
 
-var timer;
-var second_timer;
+var timer : Timer;
 var second: int = 1;
 @export var seconds: int = 90;
 
@@ -11,24 +10,18 @@ var player: Gunner;
 func _ready(): 
 	
 	timer = Timer.new()
-	second_timer = Timer.new()
-	timer.connect("timeout",_on_timer_timeout) 
-	second_timer.connect("timeout", _update_hud)
+	timer.connect("timeout",_on_timer_timeout)
 	add_child(timer) 
-	add_child(second_timer)
 	timer.start(seconds) 
-	second_timer.start(second);
+	
+func _process(delta: float) -> void:
+	if player.get_menu().is_open() != timer.paused:
+		timer.paused = player.get_menu().is_open()
+		
+	player.hud.timer.text = str(int(timer.get_time_left()))
 	
 func set_player(node: Gunner):
 	player = node;
-
-func _update_hud():
-	second_timer = Timer.new()
-	second_timer.connect("timeout", _update_hud)
-	second_timer.start(second);
-	var text = int(timer.get_time_left())
-	player.hud.timer.text = str(text);
-
 	
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
