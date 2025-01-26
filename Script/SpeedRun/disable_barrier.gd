4c63bcc1ccf816742c57b1b211ca450870ea57cb
equusligneus <reher.frederik@gmail.com>
2025-01-26 14:09:59 +0100

    Made barrier for disabling barriers

--- Script/SpeedRun/disable_barrier.gd
+++ Script/SpeedRun/disable_barrier.gd
@@ -1,9 +1,13 @@
+class_name DisableBarrier
 extends Area3D
 
 @export var _shape : CollisionShape3D
 @export var _barriers : Array[CollisionShape3D] = []
 
+@export var _temp_worker : DisableBarrier
 @export var _reenable_after := -1.0
+
+var _tween : Tween
 
 func _on_body_entered(body: Node3D):
 	if body is Gunner:
@@ -12,8 +16,11 @@ func _on_body_entered(body: Node3D):
 			barrier.disabled = true
 			barrier.get_parent_node_3d().visible = false
 		if _reenable_after > 0.0:
-			await get_tree().create_timer(_reenable_after).timeout
-			_reenable()
+			_tween = create_tween()
+			_tween.tween_interval(_reenable_after)
+			_tween.tween_callback(_reenable)
+		elif _temp_worker != null:
+			_temp_worker._tween.kill()
 
 func _reenable():
 	_shape.disabled = false
